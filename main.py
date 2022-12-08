fc7629ffe8ab016ed03e8c83e3004ce1012638e9
Ashish Sahu <86140629+a0s21en5@users.noreply.github.com>
2022-12-08 10:00:31 +0000

    Update Password Generator

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import random
 
 characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_"
 
-password_length = int(input("Enter The Length of Required Password: "))
+while True:
+    password_length = int(input("Enter The Length of Required Password: "))
 
-password_count = int(input("Enter The Number of Required Password: "))
+    password_count = int(input("Enter The Number of Required Password: "))
 
-for i in range(0, password_count):
-    password = ""
-    for j in range(0, password_length):
-        password_characters = random.choice(characters)
-        password = password + password_characters
-    print("The Generated Password is ", password)
+    for i in range(0, password_count):
+        password = ""
+        for j in range(0, password_length):
+            password_characters = random.choice(characters)
+            password = password + password_characters
+        print("The Generated Password is ", password)
+
+    repeat = input(
+        "Do You Want to Generate More Passwords? Then Type 'YES' And 'NO'")
+    if repeat == "no" or repeat == "NO":
+        break
+
+print("Thank You!")
